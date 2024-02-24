eafcbf1e104dfc9be4d4d347cee1026654921002
srcho01 <seorim5306@gmail.com>
2024-02-25 01:26:17 +0900

    Set window title

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Window(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.setWindowTitle("Vocabulary List Maker")
         
         ### handler ###  
         # file input
